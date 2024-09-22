422b7de46d48f02cce1348e82c2c25c4fae3ea79
Artem Savelev <h3xsoul@gmail.com>
2024-09-22 13:02:18 -0500

    Parse HTTP status line and headers

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,19 @@
 const std = @import("std");
 
 const net = std.net;
-// const StreamServer = net.Stream;
 const Server = net.Server;
 const Stream = net.Stream;
 const Address = net.Address;
+
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
 
 pub const io_mode = .evented;
 // TCP Listener + HTTP protocol + handlers
 
 pub fn main() !void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     const address = try Address.resolveIp("127.0.0.1", 8080);
     var server = try address.listen(.{});
     defer server.deinit();
@@ -19,11 +23,42 @@ pub fn main() !void {
     while (true) {
         var connection = try server.accept();
         std.debug.print("Incoming: {}\n", .{connection.address});
-        try handler(&connection.stream);
+        try handler(allocator, &connection.stream);
     }
 }
 
-fn handler(stream: *net.Stream) !void {
+fn handler(allocator: std.mem.Allocator, stream: *net.Stream) !void {
     defer stream.close();
-    try stream.writer().print("Hello world from zig", .{});
+    const first_line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+    var first_line_iter = std.mem.split(u8, first_line, " ");
+
+    const method = first_line_iter.next().?;
+    const uri = first_line_iter.next().?;
+    const version = first_line_iter.next().?;
+
+    var headers = std.StringHashMap([]const u8).init(allocator);
+
+    while (true) {
+        const line = try stream.reader()
+            .readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+        if (line.len == 1 and std.mem.eql(u8, line, "\r")) break;
+        var line_iter = std.mem.split(u8, line, ":");
+        const key = line_iter.next().?;
+        var value = line_iter.next().?;
+        if (value[0] == ' ') value = value[1..];
+        try headers.put(key, value);
+    }
+
+    std.debug.print("method: {s}\nuri: {s}\nversion: {s}\n", .{
+        method,
+        uri,
+        version,
+    });
+
+    var headers_iter = headers.iterator();
+
+    std.debug.print("Headers:\n", .{});
+    while (headers_iter.next()) |entry| {
+        std.debug.print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
 }
